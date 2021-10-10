6afd21165742400332dd1d71e4f48153e1c04db7
Whitewolf185 <matvey2001xxl1976@mail.ru>
2021-10-10 10:31:20 +0300

    сверстано

---
+++ Application/local_server/WebApp/js/MyScripts.js
@@ -0,0 +1,166 @@
+let btn1 = false;
+function val1Btn_onClick(){
+    let arr = document.getElementById("plus1");
+    if (!btn1) {
+        arr.style.opacity = "0";
+        // arr.style.transition = "opacity 0.5s easy";
+        btn1 = true;
+    }
+    else {
+        btn1 = false;
+        arr.style.opacity = "1";
+        // arr.style.transition = "opacity 0.5s easy";
+    }
+}
+
+
+let btn2 = false;
+function val2Btn_onClick(){
+    let arr = document.getElementById("plus2");
+    if (!btn2) {
+        arr.style.opacity = "0";
+        // arr.style.transition = "opacity 0.5s easy";
+        btn2 = true;
+    }
+    else {
+        btn2 = false;
+        arr.style.opacity = "1";
+        // arr.style.transition = "opacity 0.5s easy";
+    }
+}
+
+let btn3 = false;
+function val3Btn_onClick(){
+    let arr = document.getElementById("plus3");
+    if (!btn3) {
+        arr.style.opacity = "0";
+        // arr.style.transition = "opacity 0.5s easy";
+        btn3 = true;
+    }
+    else {
+        btn3 = false;
+        arr.style.opacity = "1";
+        // arr.style.transition = "opacity 0.5s easy";
+    }
+}
+
+
+let btn4 = false;
+function val4Btn_onClick(){
+    let arr = document.getElementById("plus4");
+    if (!btn4) {
+        arr.style.opacity = "0";
+        // arr.style.transition = "opacity 0.5s easy";
+        btn4 = true;
+    }
+    else {
+        btn4 = false;
+        arr.style.opacity = "1";
+        // arr.style.transition = "opacity 0.5s easy";
+    }
+}
+
+let btn5 = false;
+function val5Btn_onClick(){
+    let arr = document.getElementById("plus5");
+    if (!btn5) {
+        arr.style.opacity = "0";
+        // arr.style.transition = "opacity 0.5s easy";
+        btn5 = true;
+    }
+    else {
+        btn5 = false;
+        arr.style.opacity = "1";
+        // arr.style.transition = "opacity 0.5s easy";
+    }
+}
+
+let btn6 = false;
+function val6Btn_onClick(){
+    let arr = document.getElementById("plus6");
+    if (!btn6) {
+        arr.style.opacity = "0";
+        // arr.style.transition = "opacity 0.5s easy";
+        btn6 = true;
+    }
+    else {
+        btn6 = false;
+        arr.style.opacity = "1";
+        // arr.style.transition = "opacity 0.5s easy";
+    }
+}
+
+let btn7 = false;
+function val7Btn_onClick(){
+    let arr = document.getElementById("plus7");
+    if (!btn7) {
+        arr.style.opacity = "0";
+        // arr.style.transition = "opacity 0.5s easy";
+        btn7 = true;
+    }
+    else {
+        btn7 = false;
+        arr.style.opacity = "1";
+        // arr.style.transition = "opacity 0.5s easy";
+    }
+}
+
+let btn8 = false;
+function val8Btn_onClick(){
+    let arr = document.getElementById("plus8");
+    if (!btn8) {
+        arr.style.opacity = "0";
+        // arr.style.transition = "opacity 0.5s easy";
+        btn8 = true;
+    }
+    else {
+        btn8 = false;
+        arr.style.opacity = "1";
+        // arr.style.transition = "opacity 0.5s easy";
+    }
+}
+
+let btn9 = false;
+function val9Btn_onClick(){
+    let arr = document.getElementById("plus9");
+    if (!btn9) {
+        arr.style.opacity = "0";
+        // arr.style.transition = "opacity 0.5s easy";
+        btn9 = true;
+    }
+    else {
+        btn9 = false;
+        arr.style.opacity = "1";
+        // arr.style.transition = "opacity 0.5s easy";
+    }
+}
+
+let btn10 = false;
+function val10Btn_onClick(){
+    let arr = document.getElementById("plus10");
+    if (!btn10) {
+        arr.style.opacity = "0";
+        // arr.style.transition = "opacity 0.5s easy";
+        btn10 = true;
+    }
+    else {
+        btn10 = false;
+        arr.style.opacity = "1";
+        // arr.style.transition = "opacity 0.5s easy";
+    }
+}
+
+let btn11 = false;
+function val11Btn_onClick(){
+    let arr = document.getElementById("plus11");
+    if (!btn11) {
+        arr.style.opacity = "0";
+        // arr.style.transition = "opacity 0.5s easy";
+        btn11 = true;
+    }
+    else {
+        btn11 = false;
+        arr.style.opacity = "1";
+        // arr.style.transition = "opacity 0.5s easy";
+    }
+}
