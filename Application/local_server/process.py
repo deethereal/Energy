8855ea5e691126c8bdf46427b8ae994de1a850c8
Whitewolf185 <matvey2001xxl1976@mail.ru>
2021-10-10 19:32:40 +0300

    minore update

--- Application/local_server/process.py
+++ Application/local_server/process.py
@@ -1,6 +1,6 @@
 import joblib
 import pandas as pd
-
+import numpy as np
 import json
 
 def main(args):
@@ -8,7 +8,17 @@ def main(args):
     model = joblib.load('ridge_reg.pkl')
     if args[1] == "0":
         df = pd.read_csv(args[0],sep=";")
-        result = pd.DataFrame(model.predict(scaler.transform(df.values)),columns=['valve_'+str(i) for i in range(1,13)])
+        predictions = model.predict(scaler.transform(df.values))[0]
+        ver_preds = []
+        for i in range(len(predictions)):
+            if predictions[i]>1:
+                ver_preds.append(1)
+            elif predictions[i]<0:
+                 ver_preds.append(0.1)
+            else:
+                ver_preds.append(predictions[i])
+
+        result = pd.DataFrame(np.array(ver_preds).reshape(1,-1),columns=['valve_'+str(i) for i in range(1,13)])
     else :
         with open(args[0],'r') as f:
             data = json.load(f)
