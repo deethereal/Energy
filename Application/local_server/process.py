15dcd3e578b7468467680c695902765cb611fece
denis <mistrny@gmail.com>
2021-10-10 10:57:32 +0300

    port

--- Application/local_server/process.py
+++ Application/local_server/process.py
@@ -5,7 +5,7 @@ def main(file_name):
     model = joblib.load('ridge_reg.pkl')
     df = pd.read_csv(file_name[0],sep=";")
     result = pd.DataFrame(model.predict(scaler.transform(df.values)),columns=['valve_'+str(i) for i in range(1,13)])
-    result.to_csv('result.csv')
+    result.to_json('result.json', orient = 'records')
 
 if __name__ == '__main__':
     import sys
